a6d372a90d603d513e2a2d397b165c8333518c99
AviyaVenkatraman <kaviya2825@outlook.com>
2025-04-19 16:24:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,8 +53,6 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)})
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)  # or any port, like 5000
 
